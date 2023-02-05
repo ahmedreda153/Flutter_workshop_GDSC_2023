93e1c120d5596cadc1a6c0542fce1f665de0e834
Reda <ahmedreda5338@gmail.com>
2023-02-05 23:22:14 +0200

    session1-task

--- built_in/built_in.dart
+++ built_in/built_in.dart
@@ -2,38 +2,47 @@ void main(List<String> args) {
   //! EASY
   // convert string to int
   String number = '10';
-  int number2;
+  int number2 = int.parse(number);
+  print(number2);
   // output should be 10
 
   // convert int to string
   int number3 = 10;
-  String number4;
+  String number4 = number3.toString();
+  print(number4);
   // output should be '10'
 
   // uppercase and lowercase a string
   String name = 'ahmed';
-  String upperCaseName;
-  String lowerCaseName;
+  String upperCaseName = name.toUpperCase();
+  String lowerCaseName = name.toLowerCase();
+  print(upperCaseName);
+  print(lowerCaseName);
   // output should be AHMED and ahmed
 
   // check if a string is empty or not
   String name2 = '';
   String name3 = 'ahmed';
-  bool isEmpty;
-  bool isNotEmpty;
+  bool isEmpty = name2.isEmpty;
+  bool isNotEmpty = name3.isNotEmpty;
+  print(isEmpty);
+  print(isNotEmpty);
   // output should be true and false
 
   // check if a string contains a substring
   String name4 = 'ahmed';
   String substring = 'med';
-  bool contains;
+  bool contains = name4.contains(substring);
+  print(contains);
   // output should be true
 
   // check the type of a variable
   dynamic name5 = 'ahmed';
   int number5 = 10;
-  bool isString;
-  bool isInt;
+  bool isString = name5 is String;
+  bool isInt = number5 is int;
+  print(isString);
+  print(isInt);
   // output should be true and false
 
   //! MEDIUM
@@ -43,11 +52,13 @@ void main(List<String> args) {
   Map<String, int> grades = {'Ahmed': 75, 'Youssef': 82, 'Sherif': 90};
 
   // convert map into Key- Value pairs in list without using for loop
-  List<MapEntry<String, int>> entries;
+  List<MapEntry<String, int>> entries = grades.entries.toList();
+  print(entries);
 
   // convert this list into set
   List<int> numbers = [1, 2, 3, 4, 5, 1, 2, 3];
-  Set<int> uniqueNumbers;
+  Set<int> uniqueNumbers = numbers.toSet();
+  print(uniqueNumbers);
 
   // check if "Ahmed" exsist in the map or not without using for loop
   Map<String, int> grades2 = {
@@ -58,7 +69,8 @@ void main(List<String> args) {
     'Mohamed': 82,
     'Ali': 90
   };
-  bool isAhmedExist;
+  bool isAhmedExist = grades2.containsKey("Ahmed");
+  print(isAhmedExist);
 
   // check if "Ahmed" exsist in the List or not without using for loop
 
@@ -71,7 +83,8 @@ void main(List<String> args) {
     'Ali'
   ];
 
-  bool isAhmedExistInList;
+  bool isAhmedExistInList = names.contains("Ahmed");
+  print(isAhmedExistInList);
 
   //! BONUS NULL SAFETY
   // Search for this "??" double question mark in the code and try to understand what it does
@@ -79,7 +92,8 @@ void main(List<String> args) {
   // check if the name is null or not if it is null then return "John" else return the name
   String? namee;
   String defaultName = 'John Doe';
-  String actualName;
+  String actualName = namee ?? defaultName;
+  print(actualName);
 
   // check if the name is null or not if it is null then return "John" else return the name
 }
--- where_map_foreach/map.dart
+++ where_map_foreach/map.dart
@@ -1,8 +1,15 @@
 void main(List<String> args) {
   // use map to convert every grade with A , B , C , D , F
   Map<String, int> grades = {'Ahmed': 75, 'Youssef': 82, 'Sherif': 90};
-  Map<String, String> gradesWithLetters;
+  Map<String, String> gradesWithLetters = grades.map((key, value) {
+    String letter = '';
+    if (value >= 50 && value < 60) letter = 'F';
+    if (value >= 60 && value < 70) letter = 'D';
+    if (value >= 70 && value < 80) letter = 'C';
+    if (value >= 80 && value < 90) letter = 'B';
+    if (value >= 90 && value < 100) letter = 'A';
+    return (MapEntry(key, letter));
+  });
   // output should be {'Ahmed': 'C', 'Youssef': 'B', 'Sherif': 'A'}
-  
-
+  print(gradesWithLetters);
 }
